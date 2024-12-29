d968dd5d5186bb0b3ce8937e5dcf7566c669a393
rognoni <rognoni@users.noreply.github.com>
2024-12-29 15:23:31 +0100

    Moved markdown and layout in META properties

--- mdzg.js
+++ mdzg.js
@@ -1,5 +1,18 @@
-function mdzg(layout_html, page_md) {
+function mdzg() {
     window.addEventListener("load", async (event) => {
+        const metaList = document.getElementsByTagName("meta")
+
+        for (const meta of metaList) {
+            const property = meta.getAttribute("property")
+            if (property != null) {
+                if (property == "mdzg:markdown") {
+                    var page_md = meta.getAttribute("content")
+                } else if (property == "mdzg:layout") {
+                    var layout_html = meta.getAttribute("content")
+                }
+            }
+        }
+
         const layout = await fetch(layout_html)
         const html = await layout.text()
         document.getElementById("page").outerHTML = html
